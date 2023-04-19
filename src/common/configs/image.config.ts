51c37df140dc1693aa1a9ad75dd9dd0b4e7d8e0a
FrontndDevJunaid <99185498+FrontndDevJunaid@users.noreply.github.com>
2023-04-19 13:15:16 +0500

    Updating main.yml file

--- src/common/configs/image.config.ts
+++ src/common/configs/image.config.ts
@@ -3,21 +3,20 @@ import { existsSync, mkdirSync } from 'fs';
 import { diskStorage } from 'multer';
 import { v4 as uuid } from 'uuid';
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { join } from 'path';
+
+// Define the absolute path to the uploads directory
+const UPLOADS_DIR = join(__dirname, '..', 'uploads');
 
 // Multer configuration
 export const multerConfig = {
-  // dest: process.env.UPLOAD_LOCATION,
-  dest: function (req, file, callback) {
-    callback('/uploads/');
-  },
+  dest: UPLOADS_DIR,
 };
 
 // Multer upload options
-
 export const imageValidation = {
   // Enable file size limits
   limits: {
-    // fileSize: +process.env.MAX_FILE_SIZE,
     fileSize: 10000000, // 5 Mb,
   },
   // Check the mimetypes to allow for upload
@@ -26,10 +25,10 @@ export const imageValidation = {
     if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
       if (fileSize > 10000000) {
         cb(
-          new HttpException(
-            'Image size is too large',
-            HttpStatus.NOT_ACCEPTABLE,
-          ),
+            new HttpException(
+                'Image size is too large',
+                HttpStatus.NOT_ACCEPTABLE,
+            ),
         );
       }
       // Allow storage of file
@@ -37,31 +36,31 @@ export const imageValidation = {
     } else if (file.mimetype.match(/\/(mp4)$/)) {
       if (fileSize > 2000000) {
         cb(
-          new HttpException(
-            'video size is too large',
-            HttpStatus.NOT_ACCEPTABLE,
-          ),
+            new HttpException(
+                'video size is too large',
+                HttpStatus.NOT_ACCEPTABLE,
+            ),
         );
       }
       cb(null, true);
     } else if (file.mimetype.match(/\/(pdf)$/)) {
       if (fileSize > 2000000) {
         cb(
-          new HttpException(
-            'file size is too large',
-            HttpStatus.NOT_ACCEPTABLE,
-          ),
+            new HttpException(
+                'file size is too large',
+                HttpStatus.NOT_ACCEPTABLE,
+            ),
         );
       }
       cb(null, true);
     } else {
       // Reject file
       cb(
-        new HttpException(
-          `Unsupported file type ${extname(file.originalname)}`,
-          HttpStatus.BAD_REQUEST,
-        ),
-        false,
+          new HttpException(
+              `Unsupported file type ${extname(file.originalname)}`,
+              HttpStatus.BAD_REQUEST,
+          ),
+          false,
       );
     }
   },
@@ -69,11 +68,10 @@ export const imageValidation = {
   storage: diskStorage({
     // Destination storage path details
     destination: function (req: any, file: any, callback: any) {
-      // const uploadPath = multerConfig.dest;
-      if (!existsSync('/uploads/')) {
-        mkdirSync(`'__dirname + \'/../uploads/\'`);
+      if (!existsSync(UPLOADS_DIR)) {
+        mkdirSync(UPLOADS_DIR);
       }
-      callback(null, '/uploads/');
+      callback(null, UPLOADS_DIR);
     },
     // File modification details
     filename: (req: any, file: any, cb: any) => {
